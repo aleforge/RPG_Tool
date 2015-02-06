7f9df2626d8ab32915584f5f79e8e0f8e79eed1f
unknown <Erik@PSY-LYGOTA.ad.gatech.edu>
2015-02-06 09:25:57 -0500

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by Erik on 2/6/2015.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
